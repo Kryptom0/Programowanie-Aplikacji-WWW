ce47a196fd33dc1d9a51f6f96ebd86e8ab4dd5f9
Kryptom0 <92163635+Kryptom0@users.noreply.github.com>
2023-10-25 13:29:49 +0200

    Zajecia

---
+++ www_lab4/labor_164361_1Isi.php
@@ -0,0 +1,20 @@
+<?php
+//Zadanie1
+ $nr_indeksu = '164361';
+ $nrGrupy = 'IISI';
+ echo 'Krystian Drzaszcz '.$nr_indeksu. 'grupa '.$nrGrupy.' <br /><br />';
+ echo 'Zastosowanie metody include() <br />';
+ //Zadanie2_a)
+ echo 'Zadanie 2a <br />';
+ $color = '';
+ $fruit = '';
+ echo "A $color $fruit <br />";
+ include 'vars.php';
+ echo "A $color $fruit <br />"; 
+ echo 'Zadanie 2b <br />';
+ //Zadanie2_c)
+ echo 'Zadanie 2c <br />';
+ //Zadanie2_d)
+ echo 'Zadanie 2d <br />';
+
+?>
